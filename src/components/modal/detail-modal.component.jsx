fba4823f32543405e021cddc890618b3bf3223bf
sgitwhyd <sigitwahyu.mutu@gmail.com>
2022-12-22 16:24:42 +0700

    feat: detail modal component and make it dynamic

--- src/components/modal/detail-modal.component.jsx
+++ src/components/modal/detail-modal.component.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import CloseImg from '../../assets/close.svg';
 
 const DetailModal = (props) => {
-	const { onCloseModalHandler } = props;
+	const { onCloseModalHandler, type, videoLink, desc } = props;
 	return (
 		<>
-			<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-				<div className='relative w-auto my-6 mx-auto max-w-3xl'>
+			<div className='justify-center items-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none'>
+				<div className='relative my-6 mx-auto max-w-3xl'>
 					{/*content*/}
 					<div className='border-0 rounded-[30px] shadow-md relative flex flex-col w-full bg-white outline-none focus:outline-none'>
 						{/*header*/}
 						<div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
 							<div className='w-full flex justify-between items-center'>
-								<h3 className='text-3xl font-semibold'>Story</h3>
+								<h3 className='text-3xl font-semibold'>
+									{type ? type.charAt(0).toUpperCase() + type.slice(1) : ''}
+								</h3>
 								<img
 									src={CloseImg}
 									alt='close'
@@ -20,22 +22,21 @@ const DetailModal = (props) => {
 									onClick={onCloseModalHandler}
 								/>
 							</div>
-							<button
-								className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
-								onClick={() => setShowModal(false)}>
-								<span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
-									×
-								</span>
-							</button>
 						</div>
 						{/*body*/}
-						<div className='relative p-6 flex-auto'>
-							<p className='my-4 text-slate-500 text-lg leading-relaxed'>
-								I always felt like I could do anything. That’s the main thing
-								people are controlled by! Thoughts- their perception of
-								themselves! They're slowed down by their perception of
-								themselves. If you're taught you can’t do anything, you won’t do
-								anything. I was taught I could do everything.
+						<div className='relative p-6 flex-auto max-h-[500px] overflow-hidden'>
+							<p className='my-4 text-slate-500 text-lg leading-relaxed overflow-y-auto max-h-[400px]'>
+								{type === 'video' ? (
+									<iframe
+										className='w-[600px] h-72'
+										src={videoLink}
+										title='YouTube video player'
+										frameBorder='0'
+										allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+										allowFullScreen></iframe>
+								) : (
+									desc
+								)}
 							</p>
 						</div>
 					</div>

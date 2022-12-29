d067466ff93df0091b50f0a3d7b96c8fb72847d9
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-29 16:38:15 +0700

    feat: add datas wayangs

---
+++ src/utils/datas.jsx
@@ -0,0 +1,147 @@
+import kresna from "../assets/kresna.png";
+import karna from "../assets/karna.png";
+import bima from "../assets/bima.png";
+
+const datas = [
+  {
+    id: 1,
+    name: "Kresna",
+    image: kresna,
+    object: "https://prod.spline.design/wGCnakpu5DICWg9C/scene.splinecode",
+    category: "Mahabarata",
+    origin: "Mathura",
+    desc: "Dalam budaya pewayangan Jawa, tokoh Kresna dikenal sebagai raja Dwarawati (Dwaraka), kerajaan para keturunan Yadu dan merupakan titisan Dewa Wisnu. Kresna muda bernama Narayana, adalah putra Basudewa, Raja Mandura (Mathura).",
+    descLink:
+      "https://www.suaramerdeka.com/hiburan/pr-044837418/kisah-pewayangan-mengenal-tokoh-kresna-sang-titisan-dewa-wisnu",
+    addition: [
+      {
+        type: "senjata",
+        icon: "https://i.ibb.co/8rDHtYc/sword.png",
+        name: " Cakra Sudarsana",
+        desc: "Cakra Sudarsana, dalam mitologi Hindu, adalah senjata berputar yang dahsyat berbentuk cakram dengan 108 gerigi tajam di tepinya. Senjata itu dimiliki oleh Dewa Wisnu. Cakra Sudarsana tampak dibawa di tangan kanan belakangnya, di antara empat lengannya yang lain, masing-masing membawa sangkakala (tangan kiri belakang), gada (tangan kiri depan), dan bunga padma di tangan kanan depan.",
+      },
+      {
+        type: "kisah",
+        name: "Kresna",
+        icon: "https://i.ibb.co/fQC7mvK/story.png",
+        desc: "Diceritakan bahwa Kresna mampu membunuh raja di Dwarakawestri, Prabu Kunjana Kresna/Yuda Kalakresna. Kemudian ia menjadi raja di sana. Nama musuhnya tetap dipakai olehnya shingga namanya menjadi Prabu Kresna. Raja Kresna dikenal adil, bijksana, murah hati, pandai, sakti, dan didukung oleh rakyatnya. Negara Dwarakawestri pun jadi subur, makmur dan adil merata. Karena ia menjadi titisan Dewa Wisnu, Kresna mempunyai kewajiban menjaga dunia. Ia juga menjadi penasehat para Pandawa yang merupakan masih sekeluarga dengan Kresna. Kresna dan Pandawa pernah meminta negara Astina yang direbut oleh Duryudana. Tetapai Kurawa sangat licik, Kresna diracun, dikeroyok dan dihujani senjata agar mati. Namun Kurawa tak menyangka bahwa Kresna langsung menjadi raksasa dan mengobrak-abrik Astina. Ia berhenti setelah Narada turun dari kahyangan. Selanjutnya setelah menyelesaikan kewajibannya sebagai Wisnu, Kresna menginginkan muksa. Ia bertapa dengan cara tidur di atas pohon. Kresna muksa setelah telapak kakinya terkena panah seorang pemburu, Ki Jara. Kresna muksa dalam wujud sebagai Wisnu.",
+      },
+      {
+        type: "video",
+        icon: "https://i.ibb.co/QQGYpd5/video.png",
+        name: "Kresna",
+        videoUrl: "https://www.youtube.com/embed/9um16c8M5dQ",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Karna",
+    image: karna,
+    object: "https://prod.spline.design/k7hCkU6IhJ55CUVb/scene.splinecode",
+    category: "Mahabarata",
+    origin: "Kerajaan Angga",
+    desc: "Karna adalah nama Raja Angga dalam wiracarita Mahabharata. Ia menjadi pendukung utama pihak Korawa dalam perang besar melawan Pandawa. Karna merupakan kakak tertua dari tiga di antara lima Pandawa: Yudistira, Bimasena, dan Arjuna. Dalam bagian akhir perang besar tersebut, Karna diangkat sebagai panglima pihak Korawa, dan akhirnya gugur di tangan Arjuna.",
+    descLink: "https://id.wikipedia.org/wiki/Karna",
+    addition: [
+      {
+        type: "senjata",
+        icon: "https://i.ibb.co/8rDHtYc/sword.png",
+        name: "Indrastra (Wasawisakti)",
+        desc: "Indrastra adalah senjata mitos dewa India Indra yang dipanggil oleh karakter utama Arjuna berkali-kali dalam perang Mahabharata. Ketika senjata ilahi ditembakkan, itu berubah menjadi banyak panah yang mampu membunuh banyak orang.",
+      },
+      {
+        type: "kisah",
+        name: "Karna",
+        icon: "https://i.ibb.co/fQC7mvK/story.png",
+        desc: "Mahabharata bagian pertama atau Adiparwa mengisahkan seorang putri bernama Kunti yang pada suatu hari ditugasi menjamu seorang pendeta tamu ayahnya, yaitu Resi Durwasa. Atas jamuan itu, Durwasa merasa senang dan menganugerahi Kunti sebuah ilmu kesaktian bernama Adityahredaya, semacam mantra untuk memanggil dewa. Pada suatu hari, Kunti mencoba mantra tersebut setelah melakukan puja di pagi hari. Ia mencoba berkonsentrasi kepada Dewa Surya, dan sebagai akibatnya, sang dewa matahari tersebut muncul untuk memberinya seorang putra, sebagaimana fungsi mantra yang diucapkan Kunti. Kunti menolak karena ia sebenarnya hanya ingin mencoba keampuhan Adityahredaya. Surya menyatakan dengan tegas bahwa Adityahredaya bukanlah mainan. Sebagai konsekuensinya, Kunti pun mengandung. Namun, Surya juga membantunya segera melahirkan bayi tersebut. Surya kembali ke kahyangan setelah memulihkan kembali keperawanan Kunti. Dalam bahasa Sanskerta kata karṇa bermakna 'telinga'. Hal ini mengakibatkan muncul mitos bahwa Karna lahir melalui telinga Kunti. Namun, Karna juga dapat bermakna 'mahir' atau 'terampil'. Kiranya nama Karna ini baru dipakai setelah Basusena atau Radheya dewasa dan menguasai ilmu memanah dengan sempurna.",
+      },
+      {
+        type: "video",
+        icon: "https://i.ibb.co/QQGYpd5/video.png",
+        name: "Karna",
+        videoUrl: "https://www.youtube.com/embed/QwAG9d-s8DI",
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    name: "Pandu",
+    image: bima,
+    object: "https://prod.spline.design/xUeGfhf473BLWcAx/scene.splinecode",
+    category: "Mahabarata",
+    origin: "Hastinapura",
+    desc: "Pandu adalah nama tokoh dalam wiracarita Mahabharata, ayah dari para Pandawa. Pandu merupakan anak kedua dari tiga bersaudara; kakaknya Dretarastra, sedangkan adiknya Widura. Menurut Mahabharata, Dretarastra merupakan pewaris takhta kerajaan Kuru dengan pusat pemerintahan di Hastinapura.",
+    descLink: "https://id.wikipedia.org/wiki/Pandu",
+    addition: [
+      {
+        type: "senjata",
+        icon: "https://i.ibb.co/8rDHtYc/sword.png",
+        name: "Tidak ada",
+        desc: "Tidak memiliki senjata apapun",
+      },
+      {
+        type: "kisah",
+        name: "Pandu",
+        icon: "https://i.ibb.co/fQC7mvK/story.png",
+        desc: "Dikisahkan bahwa saat berburu di hutan, tanpa sengaja Pandu memanah seorang resi bernama Kindama yang sedang bersenggama dalam wujud rusa. Atas perbuatan tersebut, sang resi mengutuk Pandu agar kelak ia meninggal seketika apabila bersenggama dengan wanita. Maka dari itu, Pandu tidak bisa memiliki anak dengan cara bersenggama. Setelah dikutuk Resi Kindama, Pandu merasa bahwa perannya sebagai raja telah sia-sia apabila tidak mampu memiliki keturunan. Maka ia memutuskan untuk meninggalkan istana bersama kedua istrinya dan hidup seperti pertapa, sedangkan takhta kerajaan diserahkan kepada kakaknya, Dretarastra. Menurut kitab Adiparwa, Pandu dan kedua istrinya menuju hutan di wilayah perbukitan Satasringga. Di dalam hutan, Kunti teringat akan pengalamannya saat masih muda, ketika ia mendapat anugerah berupa pengetahuan tentang mantra sakti dari Resi Durwasa yang berguna untuk memanggil dewa tertentu, dan pengguna mantra berhak memperoleh keturunan dari setiap dewa yang dipanggil. Kunti pun memberi tahu Pandu tentang hal itu, dan atas bujukan Pandu maka ia memanggil tiga dewa: Yama, Bayu, dan Indra. Masing-masing dewa menganugerahkan seorang putra: Yudistira, Bima, dan Arjuna. Karena anjuran Pandu, Kunti membantu Madri untuk memperoleh keturunan dari dewa tertentu. Madri pun memanggil dewa kembar Aswin. Dari dewa kembar tersebut, Madri menerima putra kembar, yang diberi nama Nakula dan Sadewa. Kelima putra pandu dikenal sebagai Pandawa. Berita kelahiran mereka disampaikan ke Hastinapura. Dengan demikian, Pandu memiliki pewaris yang sah.",
+      },
+      {
+        type: "video",
+        icon: "https://i.ibb.co/QQGYpd5/video.png",
+        name: "Pandu",
+        videoUrl: "https://www.youtube.com/embed/E0LFae0Dd5s",
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    name: "Pandu",
+    image: bima,
+    object: "https://prod.spline.design/wGCnakpu5DICWg9C/scene.splinecode",
+    category: "Mahabarata",
+    origin: "Hastinapura",
+    desc: "Pandu adalah nama tokoh dalam wiracarita Mahabharata, ayah dari para Pandawa. Pandu merupakan anak kedua dari tiga bersaudara; kakaknya Dretarastra, sedangkan adiknya Widura. Menurut Mahabharata, Dretarastra merupakan pewaris takhta kerajaan Kuru dengan pusat pemerintahan di Hastinapura.",
+    descLink: "https://id.wikipedia.org/wiki/Pandu",
+    addition: [
+      {
+        type: "senjata",
+        icon: "https://i.ibb.co/8rDHtYc/sword.png",
+        name: "Tidak ada",
+        desc: "Tidak memiliki senjata apapun",
+      },
+      {
+        type: "kisah",
+        name: "Pandu",
+        icon: "https://i.ibb.co/fQC7mvK/story.png",
+        desc: "Dikisahkan bahwa saat berburu di hutan, tanpa sengaja Pandu memanah seorang resi bernama Kindama yang sedang bersenggama dalam wujud rusa. Atas perbuatan tersebut, sang resi mengutuk Pandu agar kelak ia meninggal seketika apabila bersenggama dengan wanita. Maka dari itu, Pandu tidak bisa memiliki anak dengan cara bersenggama. Setelah dikutuk Resi Kindama, Pandu merasa bahwa perannya sebagai raja telah sia-sia apabila tidak mampu memiliki keturunan. Maka ia memutuskan untuk meninggalkan istana bersama kedua istrinya dan hidup seperti pertapa, sedangkan takhta kerajaan diserahkan kepada kakaknya, Dretarastra. Menurut kitab Adiparwa, Pandu dan kedua istrinya menuju hutan di wilayah perbukitan Satasringga. Di dalam hutan, Kunti teringat akan pengalamannya saat masih muda, ketika ia mendapat anugerah berupa pengetahuan tentang mantra sakti dari Resi Durwasa yang berguna untuk memanggil dewa tertentu, dan pengguna mantra berhak memperoleh keturunan dari setiap dewa yang dipanggil. Kunti pun memberi tahu Pandu tentang hal itu, dan atas bujukan Pandu maka ia memanggil tiga dewa: Yama, Bayu, dan Indra. Masing-masing dewa menganugerahkan seorang putra: Yudistira, Bima, dan Arjuna. Karena anjuran Pandu, Kunti membantu Madri untuk memperoleh keturunan dari dewa tertentu. Madri pun memanggil dewa kembar Aswin. Dari dewa kembar tersebut, Madri menerima putra kembar, yang diberi nama Nakula dan Sadewa. Kelima putra pandu dikenal sebagai Pandawa. Berita kelahiran mereka disampaikan ke Hastinapura. Dengan demikian, Pandu memiliki pewaris yang sah.",
+      },
+      {
+        type: "video",
+        icon: "https://i.ibb.co/QQGYpd5/video.png",
+        name: "Pandu",
+        videoUrl: "https://www.youtube.com/embed/E0LFae0Dd5s",
+      },
+    ],
+  },
+];
+
+function getData(id) {
+  if (!id) {
+    return null;
+  }
+  const filteredData = datas.filter((item) => item.id === id);
+
+  if (!filteredData.length) {
+    return null;
+  }
+
+  return filteredData[0];
+}
+
+const datasModule = { datas, getData };
+
+export default datasModule;
